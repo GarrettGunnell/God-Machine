70582c1bb44392b4ca0bc899c9fcdca897ba0184
Garrett Gunnell <gunnellg@oregonstate.edu>
2025-08-07 20:34:49 -0700

    Change button appearance on hover

--- Assets/Scripts/UI/acerola_panel.gd
+++ Assets/Scripts/UI/acerola_panel.gd
@@ -1,15 +1,20 @@
 extends Panel
 class_name AcerolaPanel
 
+var panel_style : StyleBoxFlat
+
+func _init() -> void:
+	panel_style = StyleBoxFlat.new()
+
+	panel_style.bg_color = Color(1.0, 1.0, 1.0, 0.0)
+	add_theme_stylebox_override("panel", panel_style)	
 
 func _on_mouse_entered() -> void:
-	print("enter")
-	pass # Replace with function body.
+	panel_style.bg_color = Color(1.0, 1.0, 1.0, 0.2)
 
 
 func _on_mouse_exited() -> void:
-	print("exit")
-	pass # Replace with function body.
+	panel_style.bg_color = Color(1.0, 1.0, 1.0, 0.0)
 
 
 func _on_gui_input(event: InputEvent) -> void:
