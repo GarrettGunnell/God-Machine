e4c93ccd213c77f62d525f15170b2bfbec7d6527
Garrett Gunnell <gunnellg@oregonstate.edu>
2025-08-09 18:21:38 -0700

    Refactor acerola panel and implement draggable behavior

--- Assets/Scripts/UI/Panel/acerola_panel.gd
+++ Assets/Scripts/UI/Panel/acerola_panel.gd
@@ -1,13 +1,19 @@
 extends Panel
 class_name AcerolaPanel
 
+
 var panel_style : StyleBoxFlat
+var draggable = false
+var pressed = false
+var cached_pos = Vector2.ZERO
+
 
 func _init() -> void:
 	panel_style = StyleBoxFlat.new()
 
 	panel_style.bg_color = Color(1.0, 1.0, 1.0, 0.0)
 	add_theme_stylebox_override("panel", panel_style)	
+
 
 func _on_mouse_entered() -> void:
 	panel_style.bg_color = Color(1.0, 1.0, 1.0, 0.2)
@@ -19,13 +25,34 @@ func _on_mouse_exited() -> void:
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if event.pressed:
+			pressed = true
+			cached_pos = get_global_mouse_position()
 			panel_style.bg_color = Color(1.0, 1.0, 1.0, 0.0)
-
-		if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+		else:
+			pressed = false
 			panel_style.bg_color = Color(1.0, 1.0, 1.0, 0.2)
-			on_pressed()
+
+			if draggable:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+				get_node("/root").warp_mouse(cached_pos)
+
+		if event.pressed:
+			if event.button_index == MOUSE_BUTTON_LEFT: 
+				on_pressed()
+			if event.button_index == MOUSE_BUTTON_RIGHT: 
+				on_alternate_pressed()
+			
+	if event is InputEventMouseMotion && pressed && draggable:
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		on_drag(event.relative)
 
 
 func on_pressed() -> void:
+	pass
+
+func on_alternate_pressed() -> void:
+	pass
+
+func on_drag(motion : Vector2) -> void:
 	pass
--- Assets/Scripts/neighborhood_wizard.gd
+++ Assets/Scripts/neighborhood_wizard.gd
@@ -12,3 +12,11 @@ func get_spawn_range() -> Vector2i:
 
 func get_stable_range() -> Vector2i:
 	return neighborhood.get_stable_range()
+
+
+func add_to_spawn_range(v : Vector2i) -> void:
+	neighborhood.add_to_spawn_range(v)
+
+
+func add_to_stable_range(v : Vector2i) -> void:
+	neighborhood.add_to_stable_range(v)
