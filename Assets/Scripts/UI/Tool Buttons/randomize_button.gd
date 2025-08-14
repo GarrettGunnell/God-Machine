6bf0a1566ae43666865078aa6c57914ccecc98d2
Garrett Gunnell <gunnellg@oregonstate.edu>
2025-08-14 01:30:39 -0700

    Random button now updates neighborhood bytes on press

--- Assets/Scripts/UI/Tool Buttons/randomize_button.gd
+++ Assets/Scripts/UI/Tool Buttons/randomize_button.gd
@@ -10,9 +10,11 @@ func on_pressed() -> void:
 	
 	var cell_coord = Vector2i(randi() % 15 - 7, randi() % 15 - 7)
 	grid.set_cell(cell_coord, 1, Vector2i(1, 0), 0)
+	get_node("../../.").encode_grid_to_neighborhood_bytes()
 
 func on_alternate_pressed() -> void:
 	randomize()
 	
 	var cell_coord = Vector2i(randi() % 15 - 7, randi() % 15 - 7)
 	grid.set_cell(cell_coord, 1, Vector2i(0, 0), 0)
+	get_node("../../.").encode_grid_to_neighborhood_bytes()
